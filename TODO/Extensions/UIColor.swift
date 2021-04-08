e2275060e29fb4163f0dedcd315333903366aaf1
Vitaly <karl_karlsson@bk.ru>
2021-04-08 09:51:51 +0300

    Чистка контроллера
    вынес hexColor в extension

--- TODO/Extensions/UIColor.swift
+++ TODO/Extensions/UIColor.swift
@@ -14,4 +14,27 @@ extension UIColor {
     }
     static let backgroundColor = UIColor.rgb(r: 31, g: 33, b: 36)
     static let darkBrown = UIColor.rgb(r: 79, g: 52, b: 11)
+    
+    
+    static func hexStringToUIColor (hex:String) -> UIColor {
+        var cString:String = hex.trimmingCharacters(in: .whitespacesAndNewlines).uppercased()
+
+        if (cString.hasPrefix("#")) {
+            cString.remove(at: cString.startIndex)
+        }
+
+        if ((cString.count) != 6) {
+            return UIColor.gray
+        }
+
+        var rgbValue:UInt64 = 0
+        Scanner(string: cString).scanHexInt64(&rgbValue)
+
+        return UIColor(
+            red: CGFloat((rgbValue & 0xFF0000) >> 16) / 255.0,
+            green: CGFloat((rgbValue & 0x00FF00) >> 8) / 255.0,
+            blue: CGFloat(rgbValue & 0x0000FF) / 255.0,
+            alpha: CGFloat(1.0)
+        )
+    }
 }
--- TODO/Screen/General/GeneralTableViewController.swift
+++ TODO/Screen/General/GeneralTableViewController.swift
@@ -40,6 +40,8 @@ class GeneralTableViewController: UITableViewController {
         super.viewWillAppear(animated)
         self.tableView.reloadData()
     }
+    
+    //MARK: - TABLE
  
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -59,7 +61,7 @@ class GeneralTableViewController: UITableViewController {
         
         cell.selectedBackgroundView = {
             let view = UIView(frame: CGRect(x: 0, y: 0, width: cell.frame.size.width, height: cell.frame.size.height))
-            view.backgroundColor = hexStringToUIColor(hex: "#fcdab7")
+            view.backgroundColor = UIColor.hexStringToUIColor(hex: "#fcdab7")
             return view
         }()
         cell.tasksNameLabel.text = Main.instance.userSession.tasks[indexPath.section].sectionTasks[indexPath.row].name
@@ -94,8 +96,7 @@ class GeneralTableViewController: UITableViewController {
         }
     }
     
-    
-    
+    //MARK: - ACTIONS
     
     @IBAction func newTaskBarButton(_ sender: UIBarButtonItem) {
         let storyboard = UIStoryboard(name: "NewTaskStoryboard", bundle: nil)
@@ -109,32 +110,5 @@ class GeneralTableViewController: UITableViewController {
         let object = Main.instance.userSession.tasks[indexPath.section].sectionTasks[indexPath.row]
         destinationViewController.task = object
         router?.present(vc: destinationViewController)
-        
     }
-    
-    
-    
-    //Функция для преобразования hex кода в UIColor
-    func hexStringToUIColor (hex:String) -> UIColor {
-        var cString:String = hex.trimmingCharacters(in: .whitespacesAndNewlines).uppercased()
-
-        if (cString.hasPrefix("#")) {
-            cString.remove(at: cString.startIndex)
-        }
-
-        if ((cString.count) != 6) {
-            return UIColor.gray
-        }
-
-        var rgbValue:UInt64 = 0
-        Scanner(string: cString).scanHexInt64(&rgbValue)
-
-        return UIColor(
-            red: CGFloat((rgbValue & 0xFF0000) >> 16) / 255.0,
-            green: CGFloat((rgbValue & 0x00FF00) >> 8) / 255.0,
-            blue: CGFloat(rgbValue & 0x0000FF) / 255.0,
-            alpha: CGFloat(1.0)
-        )
-    }
-
 }
